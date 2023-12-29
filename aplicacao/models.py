9901319c49d403b9b2951c221bbf449bbdb55a00
phanubis <phsl.math@gmail.com>
2023-12-29 10:35:49 -0300

    Criação bd cliente

--- aplicacao/models.py
+++ aplicacao/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=12)
+    codigo = models.CharField(max_length=6, unique=True)
+    usado = models.BooleanField(default=False)
