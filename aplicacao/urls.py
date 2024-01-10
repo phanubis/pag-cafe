d014f0df5a9293e88abb1e0e20821b5a6aebd4f9
phanubis <phsl.math@gmail.com>
2024-01-10 10:42:31 -0300

    sistema de login

--- aplicacao/urls.py
+++ aplicacao/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
-from .views import cadastrar
+from .views import cadastrar, lg, login_view, logout_view, adm
 
 urlpatterns = [
     path('', views.BASE, name='BASE_URL'),
     path('cad', cadastrar, name='cad'),
+    path('lg', lg, name='lg'),
+    path('login/', login_view, name='login'),
+    path('adm/', adm, name='adm'),
+    path('logout/', logout_view, name='logout'),
+
 ]
 
--- aplicacao/views.py
+++ aplicacao/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Cliente
 import uuid
@@ -27,3 +30,29 @@ def cadastrar(request):
         return HttpResponse('Cliente cadastrado com sucesso!')
 
     return render(request, 'index.html')
+
+def lg(request):
+    return render(request, 'lg.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('adm')
+        else:
+            messages.error(request, 'Credenciais inválidas, tente novamente')
+    return render(request, 'index.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('BASE_URL')
+
+
+
+@login_required
+def adm(request):
+    return render(request, 'adm.html')
+
